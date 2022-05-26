12f2364397bdd04aa986d7f4b3144e6ce6f2c59f
chris <Chrispyohong@gmail.com>
2022-05-26 11:09:48 -0700

    phase 3 User Authentication complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,36 @@
 const router = require('express').Router();
 
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       }
+//     });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// const { restoreUser } = require('../../utils/auth.js');
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
