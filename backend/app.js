ed87d34a40ab9d969aae0873c717841052507ee0
chris <Chrispyohong@gmail.com>
2022-05-26 10:33:39 -0700

    Phase 2 complete: General  Error handling finished

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 
-if(!isProduction){
+if (!isProduction) {
   //This enables cors only in development
   app.use(cors());
 }
@@ -24,19 +25,51 @@ app.use(
   helmet.crossOriginResourcePolicy({
     policy: "cross-origin"
   })
-  );
+);
 
-  // Set the _csrf Token and creates req.csrfToken method
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
-    );
+// Set the _csrf Token and creates req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
 
-    app.use(routes);
+app.use(routes);
 
-  module.exports = app;
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
+
+
+module.exports = app;
