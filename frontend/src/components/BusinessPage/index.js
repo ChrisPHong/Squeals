d33c8b63eae4c1ca71fe31c063bebca5e0efe66e
chris <Chrispyohong@gmail.com>
2022-06-01 06:57:00 -0700

    testing a bug

--- frontend/src/components/BusinessPage/index.js
+++ frontend/src/components/BusinessPage/index.js
@@ -15,7 +15,7 @@ function BusinessPage() {
         dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
     }, [dispatch]);
 
-    let userId = useSelector((state) => state.session.user.id)
+    // let userId = useSelector((state) => state.session.user.id)
     useEffect(() => {
         dispatch(loadBusinesses());
 
