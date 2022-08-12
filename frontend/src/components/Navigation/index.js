5c8a73a1f23bf2eef9cc98208d6a79e3d440fba3
chris <Chrispyohong@gmail.com>
2022-08-12 14:16:18 -0700

    styled the navbar by adding pictures and fixed the create your business

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,14 +3,16 @@ import { Link, NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import * as sessionActions from '../../store/session';
-import {login} from '../../store/session'
+import { login } from '../../store/session'
 import './Navigation.css';
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import github from './github.png'
+import linkedIn from './linkedIn.png'
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
   const dispatch = useDispatch();
-  const user = {credential:'demo@user.io', password: 'password'}
+  const user = { credential: 'demo@user.io', password: 'password' }
   const history = useHistory();
   let sessionLinks;
 
@@ -23,42 +25,45 @@ function Navigation({ isLoaded }) {
   if (sessionUser) {
     sessionLinks = (
       <div className='navbarDiv'>
-        <Link to='/businesses'><img className='navBarIcon'src='images/squeals.png'/></Link>
-        <NavLink exact to="/businesses">Businesses</NavLink>
+        <Link to='/businesses'><img className='navBarIcon' src='images/squeals.png' /></Link>
+        {/* <NavLink exact to="/businesses">Businesses</NavLink> */}
         {/* <ProfileButton user={sessionUser} /> */}
-        <button
-        className='createYourBusinessButton'
-        onClick={() => {
-          history.push('/form')
-        }}
-        to="/form">Create Your Business</button>
-        <a href='https://github.com/ChrisPHong/Squeals'>GitHub</a>
-        <a href='https://www.linkedin.com/in/christopherpyohong/'>LinkedIn</a>
+        <div className='right-side-Nav-Bar'>
+
+          <button
+            className='createYourBusinessButton'
+            onClick={() => {
+              history.push('/form')
+            }}
+            to="/form">Create Your Business</button>
+          <a href='https://github.com/ChrisPHong/Squeals'><img className='icon-image' src={`${github}`} /></a>
+          <a href='https://www.linkedin.com/in/christopherpyohong/'> <img className='icon-image' src={`${linkedIn}`} /></a>
 
 
-  <button
+          <button
             className="logOutButton"
             onClick={logout}>Log Out</button>
 
+        </div>
       </div>
     );
   } else {
     sessionLinks = (
       <>
-      <div className='navbarDiv'>
-        <Link className='squealsLink' to='/'><img className='navBarIcon'src='images/squeals.png'/></Link>
-        <NavLink className='LogInButton' to="/login">Log In</NavLink>
-        <NavLink className='SignUpButton' to="/signup">Sign Up</NavLink>
+        <div className='navbarDiv'>
+          <Link className='squealsLink' to='/'><img className='navBarIcon' src='images/squeals.png' /></Link>
+          <NavLink className='LogInButton' to="/login">Log In</NavLink>
+          <NavLink className='SignUpButton' to="/signup">Sign Up</NavLink>
 
-      {/* <button
+          {/* <button
       className='demoButton'
       onClick={async () => {
         await dispatch(login(user))
         await history.push('/businesses')
       }}>Demo User</button> */}
-        <a href='https://github.com/ChrisPHong/Squeals'>GitHub</a>
-        <a href='https://www.linkedin.com/in/christopherpyohong/'>LinkedIn</a>
-      </div>
+          <a href='https://github.com/ChrisPHong/Squeals'>GitHub</a>
+          <a href='https://www.linkedin.com/in/christopherpyohong/'>LinkedIn</a>
+        </div>
       </>
     );
   }
